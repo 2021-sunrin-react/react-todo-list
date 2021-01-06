cd046fbd556985439ff290a41b482c5b1d3d45c4
eunseo9808 <eunseo9808@naver.com>
2021-01-06 16:01:24 +0900

    first commit

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import TodoInput from './TodoInput';
+
+const Container = () => {
+    return (
+        <div className="todo">
+            <TodoInput />
+        </div>
+    )
+};
+
+export default Container;
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const TodoList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+
+export default TodoList;
